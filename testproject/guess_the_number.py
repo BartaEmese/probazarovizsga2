1e2f415629e05c32eb108d5ea363a6ad2f59e98d
Baginé Barta Emese <67435201+BartaEmese@users.noreply.github.com>
2021-08-04 22:49:39 +0200

    Add files via upload

--- testproject/guess_the_number.py
+++ testproject/guess_the_number.py
@@ -8,24 +8,47 @@ URL = "https://witty-hill-0acfceb03.azurestaticapps.net/guess_the_number.html"
 # Oldal betöltése
 driver.get(URL)
 
-tipp = [10, 20]
+number_value = [-19, 255]
+
+# A visszajelzés szöveg elemei
 deviation_l = driver.find_element_by_xpath('//p[text()="Guess lower."]')
 deviation_h = driver.find_element_by_xpath('//p[text()="Guess higher."]')
+deviation_y = driver.find_element_by_xpath('//p[text()="Yes! That is it."]')
 
+# Elemek, gombok megkeresése
 input_number = driver.find_element_by_xpath("//input")
-input_number.send_keys(tipp[0])
-time.sleep(2)
 guess = driver.find_element_by_xpath('//button[text()="Guess"]')
+restart = driver.find_element_by_xpath('//button[text()="Restart"]')
+input_number.send_keys(1)
 guess.click()
-time.sleep(2)
-if deviation_l == "Guess higher.":
-    input_number = driver.find_element_by_xpath("//input").send_keys(tipp[1])
-    guess.click()
-elif deviation_h == "Guess higher.":
-    input_number = driver.find_element_by_xpath("//input").send_keys(input_number.text - 1)
+
+i = 1
+while deviation_y != "Yes! That is it.":
+    if deviation_h.text == "Guess higher.":
+        input_number.clear()
+        input_number.send_keys(i)
+        i += 1
+        #time.sleep(1)
+        guess.click()
+    else:
+        break
+assert i == int(driver.find_element_by_xpath('//span[@class="badge ng-binding"]').text)
 
 
+time.sleep(3)
+restart.click()
+# Teszt a -19-es számmal
+input_number.send_keys(number_value[0])
+guess.click()
+assert deviation_h.text == "Guess higher."
 
-# number_of_guesses = driver.find_element_by_xpath('//span[@class="badge ng-binding"]')
-# assert number_of_guesses.text ==
-# driver.close()
+input_number.clear()
+restart.click()
+
+# Teszt a 255-es számmal
+input_number.send_keys(number_value[1])
+guess.click()
+assert deviation_l.text == "Guess lower."
+
+
+driver.close()
